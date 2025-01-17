8441f2b65e60bcec362857e35941eb282050c838
yuandapeng <18852369945@163.com>
2025-01-17 10:28:41 +0800

    feat: add production build script and refactor webpack configuration for improved clarity

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,74 @@
-// webpack.config.js
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+/**
+ * @return {import('webpack').Configuration[]}
+ */
 module.exports = function (env, argv) {
   const mode = argv.mode || "development";
+
+  const isProd = mode === "production";
+
+  const filename = isProd ? "index.production.js" : "index.development.js";
+
+  const clean = {
+    keep: isProd ? "index.development.js" : "index.production.js",
+  };
+
+  const module = {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            },
+          },
+          "ts-loader",
+        ],
+      },
+    ],
+  };
+
+  const entry = "./src/index.ts";
+
+  const resolve = {
+    extensions: [".ts", ".tsx", ".js", ".json"], // 添加更多的扩展
+  };
+
   return [
     // CJS 配置
     {
-      mode,
-      entry: "./src/index.ts",
+      mode: mode,
+      entry: entry,
       output: {
         path: path.resolve(__dirname, "dist/cjs"),
-        filename: "index.js",
+        filename: filename,
         libraryTarget: "commonjs2",
+        clean: clean,
       },
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: "babel-loader",
-                options: {
-                  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-                },
-              },
-              "ts-loader",
-            ],
-          },
-        ],
-      },
-      resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"], // 添加更多的扩展
-      },
-      plugins: [new CleanWebpackPlugin()],
+      module: module,
+      resolve: resolve,
+      plugins: [],
     },
     // ESM 配置
     {
-      mode,
-      entry: "./src/index.ts",
+      mode: mode,
+      entry: entry,
       output: {
         path: path.resolve(__dirname, "dist/esm"),
-        filename: "index.js",
+        filename: filename,
         libraryTarget: "module",
+        clean: clean,
       },
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: "babel-loader",
-                options: {
-                  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-                },
-              },
-              "ts-loader",
-            ],
-          },
-        ],
-      },
+      module: module,
       experiments: {
         outputModule: true,
       },
-      resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"], // 添加更多的扩展
-      },
-      plugins: [new CleanWebpackPlugin()],
+      resolve: resolve,
+      plugins: [],
     },
   ];
 };
