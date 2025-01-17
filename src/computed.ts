5436658408a79177bea82bd95ec9d74e420c3572
yuandapeng <18852369945@163.com>
2025-01-17 16:43:07 +0800

    feat: enhance computed and ref interfaces for improved type safety and support for writable computed properties

--- src/computed.ts
+++ src/computed.ts
@@ -1,25 +1,50 @@
 import { effect, track, trigger } from "./effect";
 import { TrackOpTypes, TriggerOpTypes } from "./operations";
+import type { Ref } from "./ref";
 
-export function computed(options) {
-  let getter, setter;
-  if (typeof options === 'function') {
+export type ComputedGetter<T> = (oldValue?: T) => T;
+export type ComputedSetter<T> = (newValue: T) => void;
+
+export interface WritableComputedOptions<T, S = T> {
+  get: ComputedGetter<T>;
+  set: ComputedSetter<S>;
+}
+
+interface ComputedRef<T> {
+  readonly value: T;
+}
+
+interface WritableComputedRef<T, S = T> extends Ref<T, S> {}
+
+export function computed<T>(getter: ComputedGetter<T>): ComputedRef<T>;
+export function computed<T, S = T>(
+  options: WritableComputedOptions<T>
+): WritableComputedRef<T, S>;
+
+export function computed<T>(
+  options: ComputedGetter<T> | WritableComputedOptions<T>
+): ComputedRef<T> | WritableComputedRef<T> {
+  let getter: ComputedGetter<T>, setter: ComputedSetter<T>;
+  if (typeof options === "function") {
     getter = options;
     setter = () => {
-      console.warn('Computed value is readonly');
+      console.warn("Computed value is readonly");
     };
   } else {
     getter = options.get;
     setter = options.set;
   }
 
-  let value;
+  let value: T;
   let dirty = true;
 
-  const effectFn = effect(() => {
+  const effectFn = effect(
+    () => {
       value = getter();
       dirty = false;
-  }, { lazy: true });
+    },
+    { lazy: true }
+  );
 
   return {
     get value() {
@@ -29,10 +54,10 @@ export function computed(options) {
       track(this, TrackOpTypes.GET, "value");
       return value;
     },
-    set value(newValue) {
+    set value(newValue: T) {
       setter(newValue);
       dirty = true;
       trigger(this, TriggerOpTypes.SET, "value");
-    }
+    },
   };
 }
